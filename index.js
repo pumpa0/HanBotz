3b996eb8a496478046ce96bd3b1e66367c9d59ce
pumpa0 <87925130+pumpa0@users.noreply.github.com>
2021-12-01 17:43:11 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -460,6 +460,12 @@ txt = `
 - *Dana:* 085731855426
 
 `
+
+           case 'donate':
+txt = `
+*「 RUNTIME 」*
+
+> Runtime : ${kyun(uptime)}${p}`
 const pebz2 = {
             contextInfo: {
             participant: '0@s.whatsapp.net',
@@ -576,14 +582,14 @@ result = `❒「  *Wiki*  」
         const trut =['Pernah suka sama siapa aja? berapa lama?','Kalau boleh atau kalau mau, di gc/luar gc siapa yang akan kamu jadikan sahabat?(boleh beda/sma jenis)','apa ketakutan terbesar kamu?','pernah suka sama orang dan merasa orang itu suka sama kamu juga?','Siapa nama mantan pacar teman mu yang pernah kamu sukai diam diam?','pernah gak nyuri uang nyokap atau bokap? Alesanya?','hal yang bikin seneng pas lu lagi sedih apa','pernah cinta bertepuk sebelah tangan? kalo pernah sama siapa? rasanya gimana brou?','pernah jadi selingkuhan orang?','hal yang paling ditakutin','siapa orang yang paling berpengaruh kepada kehidupanmu','hal membanggakan apa yang kamu dapatkan di tahun ini','siapa orang yang bisa membuatmu sange','siapa orang yang pernah buatmu sange','(bgi yg muslim) pernah ga solat seharian?','Siapa yang paling mendekati tipe pasangan idealmu di sini','suka mabar(main bareng)sama siapa?','pernah nolak orang? alasannya kenapa?','Sebutkan kejadian yang bikin kamu sakit hati yang masih di inget','pencapaian yang udah didapet apa aja ditahun ini?','kebiasaan terburuk lo pas di sekolah apa?']
 		const ttrth = trut[Math.floor(Math.random() * trut.length)]
 		truteh = await getBuffer(`https://www.linkpicture.com/q/20211130_010958.jpg`)
-        sendButLocation(from, ttrth, 'GAK JALANIN WAJIB DONATE',truteh, {quoted: mek})
+        pebz.sendMessage(from, ttrth, 'GAK JALANIN WAJIB DONATE',truteh, {quoted: mek})
 	        	break
 		case 'dare':
 		if (!isGroup) return reply('KhususGrup')
 		const dare =['Kirim pesan ke mantan kamu dan bilang "aku masih suka sama kamu','telfon crush/pacar sekarang dan ss ke pemain','pap ke salah satu anggota grup','Bilang "KAMU CANTIK BANGET NGGAK BOHONG" ke cowo','ss recent call whatsapp','drop emot "😎??" setiap ngetik di gc/pc selama 1 hari','kirim voice note bilang can i call u baby?','drop kutipan lagu/quote, terus tag member yang cocok buat kutipan itu','pake foto sule sampe 3 hari','ketik pake bahasa daerah 24 jam','ganti nama menjadi "gue anak lucinta luna" selama 5 jam','chat ke kontak wa urutan sesuai %batre kamu, terus bilang ke dia "i lucky to hv you','prank chat mantan dan bilang " i love u, pgn balikan','record voice baca surah al-kautsar','bilang "i hv crush on you, mau jadi pacarku gak?" ke lawan jenis yang terakhir bgt kamu chat (serah di wa/tele), tunggu dia bales, kalo udah ss drop ke sini','sebutkan tipe pacar mu!','snap/post foto pacar/crush','teriak gajelas lalu kirim pake vn kesini','pap mukamu lalu kirim ke salah satu temanmu','kirim fotomu dengan caption, aku anak pungut','teriak pake kata kasar sambil vn trus kirim kesini','teriak " anjimm gabutt anjimmm " di depan rumah mu','ganti nama jadi " BOWO " selama 24 jam','Pura pura kerasukan, contoh : kerasukan maung, kerasukan belalang, kerasukan kulkas, dll']
 		const der = dare[Math.floor(Math.random() * dare.length)]
 		todz = await getBuffer(`https://www.linkpicture.com/q/20211130_010958.jpg`)
-        sendButLocation(from, der, 'GAK JALANIN WAJIB DONATE',todz, {quoted: mek})
+        pebz.sendMessage(from, der, 'GAK JALANIN WAJIB DONATE',todz, {quoted: mek})
        	   break 
            case 'self':
            if (!isOwner) return reply(mess.only.ownerB)
