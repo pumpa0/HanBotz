7cdb5baed9542be3e2a74bc72de038d24b680a77
pumpa0 <87925130+pumpa0@users.noreply.github.com>
2021-12-10 15:40:58 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -47,7 +47,6 @@ let _uang = JSON.parse(fs.readFileSync('./database/uang.json'))
 let setting = JSON.parse(fs.readFileSync('./setting.json'))
 const {
 	OwnerNumber,
-	prefix,
 	lol
 } = require('./lib/config.json')
 
