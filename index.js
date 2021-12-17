a42b8ba117f5bd5b59b7070b9e68041997b99499
pumpa0 <87925130+pumpa0@users.noreply.github.com>
2021-12-18 04:53:33 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -2329,7 +2329,7 @@ case 'meme':
                    if (isLimit(sender)) return
 			        await limitAdd(sender)
                    if (args.length < 1) return reply(`[❗] Example :\n*${prefix}${command} HanBotz*`)
-                   F = body.slice(8)				    
+                   F = body.slice(10)		    
                    anu = await getBuffer(`https://api.zeks.me/api/${command}?apikey=ubtieIG43bZfHt3RSYMtLlU4MIE&text=${F}`)
                    pebz.sendMessage(from, anu, image, {caption: `HanBotz`, quoted: mek})
                    break
@@ -2337,8 +2337,8 @@ case 'meme':
                    if (isLimit(sender)) return
 			        await limitAdd(sender)
                    if (args.length < 1) return reply(`[❗] Example :\n*${prefix}${command} HanBotz*`)
-                   F = body.slice(8)				    
-                   anu = await getBuffer(`https://api.zeks.me/api/${command}?apikey=ubtieIG43bZfHt3RSYMtLlU4MIE&text=ss ${F}`)
+                   F = body.slice(10)				    
+                   anu = await getBuffer(`https://api.zeks.me/api/${command}?apikey=ubtieIG43bZfHt3RSYMtLlU4MIE&text=${F}`)
                    pebz.sendMessage(from, anu, image, {caption: `HanBotz`, quoted: mek})
                    break
 break
@@ -2352,7 +2352,7 @@ break
                     if (isLimit(sender)) return
 			       await limitAdd(sender)	
                    if (args.length < 1) return reply(`[❗] Example :\n*${prefix}${command} Han&Botz*`)
-                   var F = body.slice(9)
+                   var F = body.slice(10)
 				   var F1 = F.split("&")[0];
 				   var F2 = F.split("&")[1]; 
                    anu = await getBuffer(`https://api.zeks.me/api/${command}?apikey=ubtieIG43bZfHt3RSYMtLlU4MIE&text1=${F1}&text2=${F2}`)
@@ -2362,10 +2362,10 @@ break
                     if (isLimit(sender)) return
 			       await limitAdd(sender)	
                    if (args.length < 1) return reply(`[❗] Example :\n*${prefix}${command} Han&Botz*`)
-                   var F = body.slice(9)
+                   var F = body.slice(10)
 				   var F1 = F.split("&")[0];
 				   var F2 = F.split("&")[1]; 
-                   anu = await getBuffer(`https://api.zeks.me/api/${command}?apikey=ubtieIG43bZfHt3RSYMtLlU4MIE&text1=s ${F1}&text2=${F2}`)
+                   anu = await getBuffer(`https://api.zeks.me/api/${command}?apikey=ubtieIG43bZfHt3RSYMtLlU4MIE&text1=${F1}&text2=${F2}`)
                    pebz.sendMessage(from, anu, image, {caption: `HanBotz`, quoted: mek})
 					break
 case 'artinama':  
@@ -2384,7 +2384,7 @@ case 'artinama':
                     if (args.length < 1) return reply('MASUKKAN MIMPI MU')
                     F = body.slice(11)
                     anu = await fetchJson(`https://api.zeks.me/api/artimimpi?apikey=ubtieIG43bZfHt3RSYMtLlU4MIE&q=${F}`)
-                    anu1 = `➻ *ARTI* : ${anu.result.string}\n`
+                    anu1 = `➻ *ARTI* : ${anu.result.array}\n`
                     reply(anu1)
                     break
 
