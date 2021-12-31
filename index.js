ccf600284409e1bb4df60f1517f3470afc6fcbc2
pumpa0 <87925130+pumpa0@users.noreply.github.com>
2021-12-31 18:47:22 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -664,7 +664,7 @@ const ftex = {
 	//=====================================/
 const anem = ['anim1','anim2','anim3','anim4','anim5','anim6','anim7','anim8','anim9','anim10','anim11','anim12','anim13','anim14','anim15','anim16','anim17','anim18','anim19','anim20','anim21']
 
-const anam = anem[Math.floor(Math.random() * (anem.length))]
+let anam = anem[Math.floor(Math.random() * (anem.length))]
 	
 		
 //>>>>>>>>>>>>>[ PEMBATAS ]<<<<<<<<<<<<<\\
@@ -1044,7 +1044,7 @@ ${p}• ${prefix}report <text>${p}
           { buttonId: `${prefix}sapa`, buttonText: { displayText: '༺ HanBotz ༻' }, type: 1 }
                  ]
            
-        sendButLocation(from, tod, tod2, ${anam}, but)
+        sendButLocation(from, tod, tod2, anam, but)
            break
 case 'sapa':
 reply("hai")
